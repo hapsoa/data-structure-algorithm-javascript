cc4a138aabda3ce40af0f625e51a9f2b21f1412c
Jaejong Heo <hapsoa@gmail.com>
2019-02-03 18:32:30 +0900

    update k번째수

---
+++ programmers-coding-test/sorting/K번째수/K번째수 전략.md
@@ -0,0 +1,15 @@
+정렬은 javascript sort()를 쓰면 적절한 효율적인 알고리즘으로 sorting 해주는듯 하다.
+
+commands의 배열을 for문으로 돌면서 findSortedNumber()를 호출한다.
+
+class LimitedSorting을 만들고 이 안에 'findSortedNumber()' 메소드가 있다.
+
+
+
+
+
+
+-----
+한정된 부분의 element만 새로운 array로 따로 가져올 수 있으면 좋겠다.
+
+splice()를 쓰면 떼내 가져올 수 있지만, 해당 배열에 있는 element가 날아가지 않을까
--- programmers-coding-test/sorting/K번째수/number-k.js
+++ programmers-coding-test/sorting/K번째수/number-k.js
@@ -1,4 +1,38 @@
+const array = [1, 5, 2, 6, 3, 7, 4];
+const commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]];
+
+class LimitedSorting {
+  constructor(array) {
+    this.array = array;
+  }
+
+  findSortedNumber(command) {
+    const limitedArray = [];
+    for (let i = command[0] - 1; i < command[1]; i++) {
+      limitedArray.push(this.array[i]);
+    }
+
+    limitedArray.sort((a, b) => a - b);
+
+    // console.log(limitedArray);
+    // console.log(limitedArray[command[2] - 1]);
+    return limitedArray[command[2] - 1];
+  }
+}
+
+// const limitedSorting = new LimitedSorting(array);
+// limitedSorting.findSortedNumber([2, 5, 3]);
+
 function solution(array, commands) {
-  var answer = [];
+
+  const limitedSorting = new LimitedSorting(array);
+  const answer = [];
+
+  commands.forEach((command) => {
+    answer.push(limitedSorting.findSortedNumber(command));
+  });
+
   return answer;
 }
+
+// console.log(solution(array, commands));
