974ff21de32f48f86d304baaa3dec5d10a3fc27f
SharingSmile <hapsoa@gmail.com>
2019-02-12 02:34:01 +0900

    update

---
+++ programmers-coding-test/그래프/가장 먼 노드/farthest-node2.js
@@ -0,0 +1,21 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.links = [];
+  }
+
+  setLink(node) {
+    this.links.push(node);
+  }
+}
+
+class Graph {
+  constructor(edges) {
+
+  }
+}
+
+function solution(n, edge) {
+  let answer = 0;
+  return answer;
+}
